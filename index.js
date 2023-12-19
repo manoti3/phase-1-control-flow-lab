88f7836c25a3b406663721125fdc0738d001b86f
manoti3 <hillarymanoti0@gmail.com>
2023-12-19 12:19:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  let answer;
+
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 2500) {
+    return "No can do.";
+  } else if (feet >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result;
+
+  city === "NYC" ? (result = "Ok, sounds good.") : (result = "No go.");
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+
+
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
